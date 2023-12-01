71489966d3fc49f36fe3916fdf49ab344eaf8800
Bachar Daowd <bachar.daowd@gmail.com>
2023-12-01 08:11:24 +0100

    Create get all customers functions

---
+++ src/main/java/com/bachar/customer/CustomerJDBCDateAccessService.java
@@ -0,0 +1,77 @@
+package com.bachar.customer;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository("jdbc")
+public class CustomerJDBCDateAccessService implements CustomerDao {
+
+    final private JdbcTemplate jdbcTemplate;
+
+    public CustomerJDBCDateAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    @Override
+    public List<Customer> selectAllCustomers() {
+        var sql = """
+                SELECT id, name, email, age
+                FROM customer
+                """;
+        //rs = result set
+        RowMapper<Customer> customerRowMapper = (rs, rowNum) -> {
+            Customer customer = new Customer(
+                    rs.getLong("id"),
+                    rs.getString("name"),
+                    rs.getString("email"),
+                    rs.getInt("age")
+            );
+            return customer;
+        };
+        List<Customer> customers = jdbcTemplate.query(sql, customerRowMapper);
+        return customers;
+    }
+
+    @Override
+    public Optional<Customer> selectCustomerById(Integer id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void insertCustomer(Customer customer) {
+        var sql = """
+                INSERT INTO customer(name, email, age)
+                VALUES(?,?,?)
+                """;
+        int result = jdbcTemplate.update(sql,
+                customer.getName(),
+                customer.getEmail(),
+                customer.getAge());
+        System.out.println("insertCustomer result " + result);
+    }
+
+    @Override
+    public boolean existsPersonWithEmail(String email) {
+        return false;
+    }
+
+    @Override
+    public boolean existPersonWithId(Integer customerId) {
+        return false;
+    }
+
+    @Override
+    public void deleteCustomer(Integer customerId) {
+
+    }
+
+    @Override
+    public void updateCustomer(Customer update) {
+
+    }
+}
