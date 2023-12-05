9be796f117fa900a181367f798bb26d6959ad9b5
Bachar Daowd <bachar.daowd@gmail.com>
2023-12-05 21:49:20 +0100

    complete unit test for CustomerJPADataAccessService

--- src/test/java/com/bachar/customer/CustomerJPADataAccessServiceTest.java
+++ src/test/java/com/bachar/customer/CustomerJPADataAccessServiceTest.java
@@ -1,5 +1,6 @@
 package com.bachar.customer;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -48,45 +49,62 @@ class CustomerJPADataAccessServiceTest {
     @Test
     void insertCustomer() {
         //Given
-
+        Customer customer = new Customer(
+                Faker.instance().name().fullName(),
+                Faker.instance().internet().emailAddress(),
+                45
+        );
         //When
-
+        underTest.insertCustomer(customer);
         //Then
+        verify(customerRepository).save(customer);
     }
 
     @Test
     void existsPersonWithEmail() {
         //Given
-
+        Customer customer = new Customer(
+                Faker.instance().name().fullName(),
+                Faker.instance().internet().emailAddress(),
+                45
+        );
         //When
-
+        underTest.existsPersonWithEmail(customer.getEmail());
         //Then
+        verify(customerRepository).existsCustomerByEmail(customer.getEmail());
     }
 
     @Test
     void existPersonWithId() {
         //Given
-
+        Long id = 1L;
         //When
-
+        underTest.existPersonWithId(id);
         //Then
+        verify(customerRepository).existsCustomerById(id);
     }
 
     @Test
     void deleteCustomer() {
         //Given
-
+        Long id = 1L;
         //When
-
+        underTest.deleteCustomer(id);
         //Then
+        verify(customerRepository).deleteById(id);
     }
 
     @Test
     void updateCustomer() {
         //Given
-
+        Customer customer = new Customer(
+                Faker.instance().name().fullName(),
+                Faker.instance().internet().emailAddress(),
+                45
+        );
         //When
-
+        underTest.updateCustomer(customer);
         //Then
+        verify(customerRepository).save(customer);
     }
 }
