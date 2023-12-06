ec49d2f0c68435f9ad419a17bccd9d27b3aa98de
Bachar Daowd <bachar.daowd@gmail.com>
2023-12-06 15:02:15 +0100

    customerService unit test part1

--- src/test/java/com/bachar/customer/CustomerServiceTest.java
+++ src/test/java/com/bachar/customer/CustomerServiceTest.java
@@ -1,14 +1,20 @@
 package com.bachar.customer;
 
-import org.junit.jupiter.api.AfterEach;
+import com.bachar.exception.ResourceNotFoundException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.refEq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CustomerServiceTest {
@@ -24,29 +30,53 @@ class CustomerServiceTest {
 
     @Test
     void getAllCustomers() {
-        //Given
-
         //When
-
+        underTest.getAllCustomers();
         //Then
+        verify(customerDao).selectAllCustomers();
     }
 
     @Test
-    void getCustomer() {
+    void canGetCustomer() {
         //Given
-
+        Long id = 1L;
+        Customer customer = new Customer(id, "Bachar Daowd", "bachar@example.com", 45);
+        Mockito.when(customerDao.selectCustomerById(id)).thenReturn(Optional.of(customer));
         //When
+        Customer actual = underTest.getCustomer(id);
+        //Then
+        Assertions.assertThat(actual).isEqualTo(customer);
+    }
+
+    @Test
+    void whenGetCustomerNotFoundThrowException() {
+        //Given
+        Long id = 1L;
+        Mockito.when(customerDao.selectCustomerById(id)).thenReturn(Optional.empty());
 
         //Then
+        Assertions.assertThatThrownBy(() -> underTest.getCustomer(id))
+                .isInstanceOf(ResourceNotFoundException.class)
+                .hasMessage("Customer with id %d is not found".formatted(id));
     }
 
     @Test
     void addCustomer() {
         //Given
-
+        CustomerRegisterRequest request = new CustomerRegisterRequest("Bachar Daowd", "bachar@example.com", 45);
+        when(customerDao.existsPersonWithEmail(request.email())).thenReturn(false);
         //When
-
+        underTest.addCustomer(request);
+//        Customer customer = Customer.builder().name(request.name())
+//                .email(request.email()).age(request.age()).build();
+        ArgumentCaptor<Customer> c = ArgumentCaptor.forClass(Customer.class);
         //Then
+        verify(customerDao).insertCustomer(c.capture());
+
+        Customer value = c.getValue();
+        Assertions.assertThat(value.getName()).isEqualTo("Bachar Daowd");
+        Assertions.assertThat(value.getEmail()).isEqualTo("bachar@example.com");
+        Assertions.assertThat(value.getAge()).isEqualTo(45);
     }
 
     @Test
