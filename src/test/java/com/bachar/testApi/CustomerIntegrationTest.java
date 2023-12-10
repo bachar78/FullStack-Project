68defcf00df68a013f1a069f45b6bacd59b1e8b2
Bachar Daowd <bachar.daowd@gmail.com>
2023-12-10 09:54:56 +0100

    Setting integration test

---
+++ src/test/java/com/bachar/testApi/CustomerIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.bachar.testApi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CustomerIntegrationTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+}
